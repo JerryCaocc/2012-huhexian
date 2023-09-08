47d0b841e60c1efe4330d2d6fc352c7a74332403
Jerry Cao <jerry.caocc@hotmail.com>
2023-09-08 09:33:18 +0800

    ..

--- content-none.php
+++ content-none.php
@@ -10,11 +10,11 @@
 
 	<article id="post-0" class="post no-results not-found">
 		<header class="entry-header">
-			<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
+			<h1 class="entry-title"><?php _e( '未找到任何内容', 'twentytwelve' ); ?></h1>
 		</header>
 
 		<div class="entry-content">
-			<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'twentytwelve' ); ?></p>
+			<p><?php _e( '抱歉，没有找到任何结果，要不搜索一下吧。', 'twentytwelve' ); ?></p>
 			<?php get_search_form(); ?>
 		</div><!-- .entry-content -->
 	</article><!-- #post-0 -->
